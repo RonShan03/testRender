7100a95b153b7c5c83621734c4f01ddacff8e0fa
RonShan03 <ronshan248@gmail.com>
2025-08-26 23:15:18 -0400

    port patch fix

--- backend/testbackend.js
+++ backend/testbackend.js
@@ -1,14 +1,12 @@
-    const http = require('http');
+const http = require('http');
 
-    const hostname = '0.0.0.0'; // localhost
-    const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-    const server = http.createServer((req, res) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Hello World!\n');
-    });
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!\n');
+});
 
-    server.listen(port, hostname, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
-    });
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
